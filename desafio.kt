c19359cdb1a76cca9f7639703d903a0589b3eba1
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-11-22 09:17:16 -0300

    desafio finalizado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,67 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Level { BASICO, INTERMEDIARIO, AVANCADO }
+enum class Post { STUDENT, TEACHER }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Users(val fNome: String, val lNome: String, val email: String, val post: Post)
 
-class Usuario
+data class EducationalContent(val name: String, val duration: Int, val level: Level)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formation(val nome: String, val contents: List<EducationalContent>, val level: Level) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val registered = mutableListOf<Users>()
+   
+    fun addUser(user: Users) {
+         registered.add(user)
+    }
+   
+    fun getAddUser(){
+        for(user in registered){
+            println("|Nome: ${user.fNome+" "+user.lNome}| - |Email: ${user.email}| - (${user.post})")
+        }
+    }
+   
+    fun printEducationalContent(){
+        for(cont in contents){
+            println("${cont.name} - ${cont.duration}min - (${cont.level})")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+ 	val moduleJavascript = EducationalContent("Linguagem Javascript", 120, Level.BASICO)
+    val moduleVsCode = EducationalContent("Configurando a IDE", 60, Level.BASICO)
+  	val moduleReact = EducationalContent("A biblioteca React", 120, Level.INTERMEDIARIO)
+  	val moduleNodeJs = EducationalContent("Javascript no Back-End", 120, Level.INTERMEDIARIO)
+    val moduleNextJs = EducationalContent("Framework do momento", 180, Level.BASICO)
+   
+	val contentList = mutableListOf<EducationalContent>()
+  		contentList.add(moduleJavascript)
+  		contentList.add(moduleVsCode)
+    	contentList.add(moduleReact)
+  		contentList.add(moduleNodeJs)
+        contentList.add(moduleNextJs)
+   
+    val carlos = Users("Carlos","Silva", "carlossilva@gmail.com", Post.STUDENT)
+    val kelly = Users("Kelly","Mendes", "kellymendes@gmail.com", Post.STUDENT)
+    val maria = Users("Maria","Cristina", "mariacristina@gmail.com", Post.STUDENT)
+    val augusto = Users("Augusto","Pereira", "augustopereira@gmail.com", Post.TEACHER)
+   
+    val androidTraining = Formation("Formação Full Stack", contentList, Level.INTERMEDIARIO)
+     
+    androidTraining.addUser(carlos)
+    androidTraining.addUser(kelly)
+    androidTraining.addUser(maria)
+    androidTraining.addUser(augusto)
+   
+      println("========================================")
+      println("|${androidTraining.nome} - ${androidTraining.level}|")
+      println("========================================")
+   
+    println("Grade do curso")
+    println(androidTraining.printEducationalContent())
+    println("========================================")
+   
+    println("Usuarios cadastrados no curso")
+    println(androidTraining.getAddUser())
 }
+    
+    
